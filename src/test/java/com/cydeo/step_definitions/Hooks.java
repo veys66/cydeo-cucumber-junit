04a99b2ca256b4f3fd43df1019d21dd5990c76f8
veys66 <veysdoganer@gmail.com>
2023-02-21 22:24:26 +0300

    step_definitions

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,33 +1,66 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.BeforeStep;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import javax.swing.border.Border;
 
 /*
 In the class we will be able to pass pre- & post- conditions to each scenario and each step
  */
 public class Hooks {
     //import from io.cucumber.java not from junit
-    @Before
+    // @Before (order = 1)
     public void setupScenario(){
         System.out.println("====Setting up browser using cucumber @Before");
     }
 
+    //@Before (value = "@login", order = 2)
+    public void setupScenarioForLogins(){
+        System.out.println("====this will only apply to scenarios with @login tag");
+    }
+
+    //@Before (value = "@db", order = 0)
+    public void setupForDatabaseScenarios(){
+        System.out.println("====this will only apply to scenarios with @db tag");
+    }
+
+/*
     @After
-    public void teardownScenario(){
-        System.out.println("====Closing browser using cucumber @After");
-        System.out.println("====Scenario ended/ Take screenshot if failed!");
+    public void teardownScenario(Scenario scenario){
+
+        //scenario.isFailed() --> if scenario fails this method will return TRUE boolean value
+
+
+        if (scenario.isFailed()){
+
+            byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+
+        }
+
+
+        //BrowserUtils.sleep(5);
+        Driver.closeDriver();
+
+        //System.out.println("====Closing browser using cucumber @After");
+        //System.out.println("====Scenario ended/ Take screenshot if failed!");
     }
 
-    @BeforeStep
+    // @BeforeStep
     public void setupStep(){
-        System.out.println("-------> applying setup using @BeforeStep");
+        System.out.println("--------> applying setup using @BeforeStep");
     }
 
-    @AfterStep
+    //@AfterStep
     public void afterStep(){
-        System.out.println("---------> applying tearDown using @AfterStep");
+        System.out.println("--------> applying tearDown using @AfterStep");
     }
+
+ */
 }
--- src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -1,6 +1,7 @@
 package com.cydeo.step_definitions;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -38,5 +39,10 @@ public class Login_StepDefinitions {
     @And("user enters admin password")
     public void userEntersAdminPassword() {
         System.out.println("User enters admin password");
+    }
+
+    @Given("user is on the library page")
+    public void userIsOnTheLibraryPage() {
+        System.out.println("User is on the library page");
     }
 }
