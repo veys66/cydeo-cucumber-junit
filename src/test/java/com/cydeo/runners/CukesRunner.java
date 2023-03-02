f8660addb70e0134aec7d472d82ba28e6065d2e1
veys66 <veysdoganer@gmail.com>
2023-03-02 09:21:32 +0300

    day 18

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -7,14 +7,16 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
+                //"pretty",
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
         },
         features = "src/test/resources/features",
-        glue = "com/cydeo/step_definitions",
+        glue = "cydeo/step_definitions",
         dryRun = false,
-        tags = "@femaleScientists"
+        tags = "",
+        publish = false
 )
 
 public class CukesRunner {
